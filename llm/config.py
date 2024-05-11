f2d01c3c643ff28253af03ae0b05e1e2f58a0b81
Waqar Ahmed <waqarahmedjoyia@live.com>
2024-05-11 07:50:33 +0500

    Fix embedding model name

--- llm/config.py
+++ llm/config.py
@@ -2,7 +2,7 @@ PDF_DIR = '/app/data/pdf_files'
 CSV_DIR = '/app/data/csv_files'
 
 INDEX_DIR = '/app/data/faiss_index'
-EMBEDDING_MODEL = 'llama3'
+EMBEDDING_MODEL = 'llama3:8b'
 
 # URL for llama3 API
 LLAMA3_BASE_URL = 'http://local-llm-ollama:11434'
